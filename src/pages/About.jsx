ce897093e872773fca7436bba5e2472f906be8ca
Muhamad Fikri Ilhamsyah <fikriilhamsyah27@gmail.com>
2025-04-25 14:13:10 +0700

    About Page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+import {experiences, skills} from '../constants'
 
 const About = () => {
   return (
@@ -12,7 +16,31 @@ const About = () => {
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
 
-        <div className="mt-16 flex flex-wrap gap-12"></div>
+        <div className="mt-16 flex flex-wrap gap-12">
+          {skills.map((skill) => (
+            <div key={skill.name} className="block-container w-20 h-20">
+              <div className="btn-back rounded-xl" />
+              <div className="btn-front rounded-xl flex justify-center items-center">
+                <img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain" />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <div className="mt-5 flex flex-col gap-3 text-slate-500">
+          <p>I've worked with all sorts companies, leveling up my skills and teaming up with smart people. Here's the rundown:</p>
+        </div>
+
+        <div className="mt-12 flex">
+          <VerticalTimeline>
+            {experiences.map((experience) => (
+              
+            ))}
+          </VerticalTimeline>
+        </div>
       </div>
     </section>
   )
